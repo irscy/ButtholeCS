922bd5b667b80ed0b04644687234c2aa3f1c3544
irscy <47720070+irscy@users.noreply.github.com>
2022-06-09 13:12:40 -0500

    Nice code fixes (will fix errors later)

--- src/script/nodes/Jinx.cs
+++ src/script/nodes/Jinx.cs
@@ -8,11 +8,15 @@ namespace Butthole.Nodes
 	public class Jinx : Node2D
 	{
 		MoveController Mover;
+		[Export] Texture JinxUp;
+		[Export] Texture JinxHoriz;
 
 		public override void _Ready()
 		{
 			Mover = (MoveController)GetNode<Node2D>("/root/Main/JINX/MoveController");
-			Mover = (MoveController)GetNode<Node2D>("/root/Main/JINX/MoveController");
+			Mover.definedNode = GetNode<Node2D>(".");
+			Mover.upSpr = JinxUp;
+			Mover.horizSpr = JinxHoriz;
 			Mover.SetObjectValues();
 			Scale.x.Equals(new Vector2(0.6f, 0.35f));
 		}
