ac7acd6e736686fc0c04a7becf1b2edb7b27a339
irscy <47720070+irscy@users.noreply.github.com>
2022-06-14 18:14:13 -0500

    Read if Jinx is not flipped (NOT PLAYTESTED)

--- src/script/settings/WeaponAnchor.cs
+++ src/script/settings/WeaponAnchor.cs
@@ -7,15 +7,22 @@ namespace Butthole
     {
         Node2D Jinx;
         Node2D JinxSprite;
+        Node2D StickSprite;
 
         public override void _Ready()
         {
             Jinx = GetParent<Node2D>();
             JinxSprite = Jinx.GetChild<Sprite>(0);
+            StickSprite = GetChild<Sprite>(0);
         }
-        public override void _Process(float delta)
+        public override void _PhysicsProcess(float delta)
         {
-            
+            if(!((Sprite)JinxSprite).FlipH)
+            {
+                Position = new Vector2(-14.972f, -38);
+                ((Sprite)StickSprite).FlipH = true;
+                RotationDegrees = 11;
+            }
         }
     }
 }
